b0313788f864a9921d81534ca82481410ab50c60
Mario Izquierdo <tothemario@gmail.com>
2013-04-01 01:37:46 -0700

    Add setup and teardown methods

--- test/tinytest_test.js
+++ test/tinytest_test.js
@@ -1,121 +1,152 @@
 // Tinytest is tested with Tinytest °O°
 // Just run this file with node.js and check the output: node test/tinytest_test.js
-var tinytest = require('tinytest');
+var Tinytest = require('tinytest');
+var tinytest = new Tinytest();
 var test        = tinytest.test;
 var assert      = tinytest.assert;
 var assertError = tinytest.assertError;
-var report;
+var sub; // test to be tested
 
 tinytest.setup = function() {
-  report = tinytest.newReport(); // reset report to use in test assertions
-};
+  sub = new Tinytest();
+}
 
 test('single assert with true expression', function() {
-  test('true', function() { assert(true) }, report);
-  test('one is one', function(){ assert(1 === 1); }, report);
+  sub.test('true', function() { assert(true) });
+  sub.test('one is one', function(){ assert(1 === 1); });
 
-  assert(report.tests === 2);
-  assert(report.failures.length === 0);
+  assert(sub.report.tests === 2);
+  assert(sub.report.failures.length === 0);
 });
 
 test('single assert with false expression', function() {
-  test('false', function() { assert(false) }, report);
-  test('one is two', function() { assert(1 === 2) }, report);
+  sub.test('false', function() { assert(false) });
+  sub.test('one is two', function() { assert(1 === 2) });
 
-  assert(report.tests === 2);
-  assert(report.failures.length === 2);
+  assert(sub.report.tests === 2);
+  assert(sub.report.failures.length === 2);
 });
 
 test('failure is the test title', function() {
-  test('test title', function() {
+  sub.test('test title', function() {
     assert(false);
-  }, report);
+  });
 
-  assert(report.failures[0] == 'test title');
+  assert(sub.report.failures[0] == 'test title');
 });
 
 test('multiple asserts in the same test', function() {
-  test('with two asserts inside', function() {
+  sub.test('with two asserts inside', function() {
     assert(true);
     assert(true);
-  }, report);
+  });
 
-  assert(report.tests === 1); // multiple asserts count just like one test
-  assert(report.failures.length === 0);
+  assert(sub.report.tests === 1); // multiple asserts count just like one test
+  assert(sub.report.failures.length === 0);
 });
 
 test('multiple asserts in the same test, one assert fails', function() {
-  test('with two asserts inside', function() {
+  sub.test('with two asserts inside', function() {
     assert(true);
     assert(false);
     assert(true);
-  }, report);
+  });
 
-  assert(report.tests === 1);
-  assert(report.failures.length === 1);
-  assert(report.failures[0], 'with two asserts inside');
+  assert(sub.report.tests === 1);
+  assert(sub.report.failures.length === 1);
+  assert(sub.report.failures[0], 'with two asserts inside');
 });
 
 test('nested test functions with no failures', function() {
-  test('root test', function(){
-    test('child test 1', function(){ assert(true); }, report);
-    test('child test 2', function(){ assert(true); }, report);
+  sub.test('root test', function(){
+    sub.test('child test 1', function(){ assert(true); });
+    sub.test('child test 2', function(){ assert(true); });
     assert(true);
-  }, report);
+  });
 
-  assert(report.tests === 3); // it counts root and nested tests
-  assert(report.failures.length === 0); // no failures
+  assert(sub.report.tests === 3); // it counts root and nested tests
+  assert(sub.report.failures.length === 0); // no failures
 });
 
 test('nested test functions with failures', function() {
-  test('root test', function(){
-    test('child test 1', function(){ assert(false); }, report);
-    test('child test 2', function(){ assert(false); }, report);
+  sub.test('root test', function(){
+    sub.test('child test 1', function(){ assert(false); });
+    sub.test('child test 2', function(){ assert(false); });
     assert(false);
-  }, report);
+  });
 
-  assert(report.tests === 3); // it counts root and nested tests
-  assert(report.failures.length === 3); // no failures
-  assert(report.failures[0] == 'child test 1'); // first assertion to fail is added first in the failures list
+  assert(sub.report.tests === 3); // it counts root and nested tests
+  assert(sub.report.failures.length === 3); // no failures
+  assert(sub.report.failures[0] == 'child test 1'); // first assertion to fail is added first in the failures list
 });
 
 test('assertError should not fail if an error is raised inside', function() {
-  test('should throw an exeption', function(){
+  sub.test('should throw an exeption', function(){
     assertError(function() {
       throw new Error('kaa'); // throw an Error
     });
-  }, report);
-  test('should throw an exeption', function(){
+  });
+  sub.test('should throw an exeption', function(){
     assertError(function() {
       throw 'kaa'; // throw a String
     });
-  }, report);
+  });
 
-  assert(report.tests === 2);
-  assert(report.failures.length === 0); // no failures
+  assert(sub.report.tests === 2);
+  assert(sub.report.failures.length === 0); // no failures
 })
 
 test('assertError should add a failure if no error was raised inside', function() {
-  test('should throw an exeption', function(){
+  sub.test('should throw an exeption', function(){
     assertError(function() {
       // stuff but no errors
     });
-  }, report);
+  });
 
-  assert(report.tests === 1);
-  assert(report.failures.length === 1); // no error was raised
+  assert(sub.report.tests === 1);
+  assert(sub.report.failures.length === 1); // no error was raised
 });
 
 test('assertError should return the raised error', function() {
-  test('should throw a "not my problem" exception', function() {
+  sub.test('should throw a "not my problem" exception', function() {
     var err = assertError(function() {
       throw "not my problem";
     });
     assert(err === "not my problem"); // use this error to add more asserts
-  }, report);
+  });
 
-  assert(report.tests === 1);
-  assert(report.failures.length === 0);
+  assert(sub.report.tests === 1);
+  assert(sub.report.failures.length === 0);
 });
+
+test('setup is called on each test case', function() {
+  var counter = 0;
+  sub.setup = function() { counter ++; }
+  sub.test('counter plus', function(){
+    assert(counter === 1);
+  });
+  sub.test('counter plus', function(){
+    assert(counter === 2);
+  });
+
+  assert(sub.report.tests === 2);
+  assert(sub.report.failures.length === 0);
+  assert(counter === 2);
+});
+
+test('teardown is called after each test case', function() {
+  var counter = 0;
+  sub.teardown = function() { counter ++; }
+  sub.test('counter plus', function(){
+    assert(counter === 0); // teardown still not called
+  });
+  sub.test('counter plus', function(){
+    assert(counter === 1);
+  });
+
+  assert(sub.report.tests === 2);
+  assert(sub.report.failures.length === 0);
+  assert(counter === 2);
+})
 
 console.log(tinytest.report.toString());
--- tinytest.js
+++ tinytest.js
@@ -1,62 +1,74 @@
-(function(tinytest){
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-  tinytest.setup = function() {};    // override to execute before each test
-  tinytest.teardown = function() {}; // override to execute after each test
+// class Tinytest
+var Tinytest = (function() {
+  function Tinytest() {
+    this.report = new TinytestReport(); // Default report
+
+    // Bind scopes to ensure "this" keeps being "this"
+    this.test = __bind(this.test, this);
+  }
+
+  Tinytest.prototype.setup = function() {};    // override to execute before each test
+  Tinytest.prototype.teardown = function() {}; // override to execute after each test
 
   // Name a test case and group some assertions on it
-  tinytest.test = function(title, testFunc, report) {
-    report || (report = tinytest.report); // use another report to isolate test cases
+  Tinytest.prototype.test = function(title, testFunc) {
     try {
-      tinytest.setup(); // execute before each test
+      this.setup(); // execute before each test
       testFunc();
     } catch (err) {
       if (err === 'tinytest failure') {
-        report.failures.push(title); // special error used to catch assert failures
+        this.report.failures.push(title); // special error used to catch assert failures
       } else {
         throw err; // there was an unexpected error
       }
     } finally {
-      tinytest.teardown(); // execute after each test
-      report.tests++; // ensure count tests
+      this.report.tests++; // ensure count tests
+      this.teardown(); // execute after each test
     }
   };
 
   // Check that some expression is true. Use inside a testFunc.
-  tinytest.assert = function(expr) {
+  Tinytest.prototype.assert = function(expr) {
     if (!expr) throw 'tinytest failure';
   };
 
   // Check that a exception is thrown.
-  tinytest.assertError = function(testFunc) {
+  Tinytest.prototype.assertError = function(testFunc) {
     try {
       testFunc();
     } catch (err) {
       return err; // use the returned error to assert the error object
     }
-    tinytest.assert(false); // add a failure if the error was not thrown
+    Tinytest.prototype.assert(false); // expected error not thrown
   };
 
-  // Generate a new report to be used by tests.
-  // You normally don't need to use this function, because a default report is generated in tinytest.report
-  tinytest.newReport = function() {
-    return {
-      tests: 0,
-      failures: [],
-      toString: function() {
-        var str = '';
-        for (i in this.failures) str += 'Failure in "' + this.failures[i] + '"\n'; // failure test titles
-        str += "" + this.tests + " tests"; // number of tests
-        str += ", " + this.failures.length + " failures"; // number of failures
-        str += this.failures.length === 0 ? ' [OK]' : ' [ERROR]'
-        return str;
-      },
-      toHTML: function() {
-        return this.toString().replace('\n', '<br/>'); // use BR tags instead of \n line-breaks
-      }
-    }
-  };
+  return Tinytest;
+})();
 
-  // Default report
-  tinytest.report = tinytest.newReport();
+// Report is used to store failures and print results of a test suite
+var TinytestReport = (function() {
+  function TinytestReport() {
+    this.tests = 0;
+    this.failures = [];
+  }
+  TinytestReport.prototype.toString = function() {
+    var str = '';
+    for (i in this.failures) str += 'Failure in "' + this.failures[i] + '"\n'; // failure test titles
+    str += "" + this.tests + " tests"; // number of tests
+    str += ", " + this.failures.length + " failures"; // number of failures
+    str += this.failures.length === 0 ? ' [OK]' : ' [ERROR]'
+    return str;
+  },
+  TinytestReport.prototype.toHTML = function() {
+    return this.toString().replace('\n', '<br/>'); // use BR tags instead of \n line-breaks
+  }
+  return TinytestReport;
+})();
 
-})(typeof exports === 'undefined' ? this['tinytest']={} : exports);
+if (typeof exports === 'undefined') {
+  window.Tinytest = Tinytest; // browser
+} else {
+  module.exports = Tinytest; // node.js (require)
+}
